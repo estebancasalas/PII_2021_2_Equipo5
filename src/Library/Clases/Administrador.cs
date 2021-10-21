0337c14a46433ee72e4b286768ef29fea60a43f4
SantiRDL <santiago.rodriguezdl@correo.ucu.edu.uy>
2021-10-21 09:24:14 -0300

    Rama de Santiago
    Incluye: Administrador, IUsuario, DatosDeMateriales, Emprendedor, Empresa

---
+++ src/Library/Clases/Administrador.cs
@@ -0,0 +1,7 @@
+
+class Administrador : IUsuario
+{ 
+    public string id {get; set;}
+    public string nombre {get; set;}
+
+}
---
+++ src/Library/Clases/Empresa.cs
@@ -0,0 +1,14 @@
+/// <summary>
+/// Clase con los datos de la empresa. Incluye:
+/// -id (private string)
+/// -nombre (private string)
+/// -ubicacion (private string)
+/// -rubro (private string)
+/// </summary>
+public class Empresa
+{
+    private string id {get;}
+    private string nombre {get;}
+    private string ubicacion {get;}
+    private string rubro {get;}
+}
