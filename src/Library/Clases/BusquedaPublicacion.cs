6da6e3bf514d3d705172a4fd9a6c1c354440d1b8
estebancasalas <e.casalas@outlook.com>
2021-10-21 20:11:34 -0300

    BuscarPublicaiones v1

---
+++ src/Library/Clases/BusquedaPublicacion.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+namespace ClassLibrary
+{
+    /// <summary>
+    /// Busqueda por palabras clave. Recorre la lista de todas las publicaciones y devuelve una lista con las coincidencias.
+    /// </summary>
+    public class BusquedaKeyWord : Busqueda
+    {
+        public static List<Publicacion> Buscar(string tipoDeBusqueda, string input)
+        {
+            List<Publicacion> result = new List<Publicacion>();
+            
+            //Pedir las palabras clave
+
+            List<string> keyWords = new List<string>();
+
+            foreach (Publicacion publicacion in RegistroPublicaciones)
+            {
+                bool found = false;
+                int keyWordsPos = 0;
+                while (!found && keyWordsPos < keyWords.Count)
+                {
+                    if (publicacion.PalabrasClave.Contains(keyWords[keyWordsPos]))
+                    {
+                        result.Add(publicacion);
+                        keyWordsPos++ ;
+                        found = true;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
