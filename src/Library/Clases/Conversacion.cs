54ef94c67e802a4d58ae30500d274f39c7b4de0a
Juan Cosentino <juanpecose@hotmail.com>
2021-10-21 21:14:47 -0300

    creacion clases

---
+++ src/Library/Clases/Conversacion.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    public class Conversacion
+    {
+        private int id {get; set;}
+
+        private List<string> mensajes = new List<string>();
+
+        void AgregarMensaje(string mensaje)
+        {
+            mensajes.Add(mensaje);
+        }
+    }
+}
---
+++ src/Library/Comandos/CrearPublicacion.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace ConsoleApplication
+{
+    public class CrearPublicacion : IComandos
+    {
+        public string nombre = "/CrearPublicacion";
+        public string titulo;
+        public DatosDeMateriales datosDeMateriales;
+        public string PalabrasClave;
+        public string FrecuenciaDeDisponibilidad;
+
+        public void EjecutarComando()
+        {
+            Console.WriteLine("Ingrese título: ");
+            string titulo = Console.ReadLine();
+            Console.WriteLine("Ingrese los datos de su material: ");
+            DatosDeMateriales datosDeMateriales = Console.ReadLine();
+            Console.WriteLine("Ingrese palabras clave: ");
+            string PalabrasClave = Console.ReadLine();
+            Console.WriteLine("Ingrese la frecuencia de disponibilidad: ");
+            string FrecuenciaDeDisponibilidad = Console.ReadLine();
+            
+            Publicacion publicacion = new Publicacion(titulo, datosDeMateriales, PalabrasClave, FrecuenciaDeDisponibilidad);
+        }
+    }
+}
