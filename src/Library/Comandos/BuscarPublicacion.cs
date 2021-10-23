04c4e73dcdebcd646cb0d60f27e2b817ebedf523
estebancasalas <e.casalas@outlook.com>
2021-10-22 22:00:23 -0300

    agregue namespace a IUsuario

--- src/Library/Comandos/BuscarPublicacion.cs
+++ src/Library/Comandos/BuscarPublicacion.cs
@@ -1,14 +1,19 @@
-namespace ClassLibrary
+using System;
+
+namespace ConsoleApplication
 {
     /// <summary>
     /// Pregunta por el tipo de busqueda, pide a la clase correspondiente que la realice y devuelve una lista con las coincidencias.
     /// Ademas, se debera preguntar por los parametros necesarios para la busqueda.
     /// En caso de una busqueda por mas de un tipo, se realizan todas las busquedas y de devuelve las coincidencias comunes a todas las busquedas.
     /// </summary>
-    public class BuscarPublicacion
+    public class BuscarPublicacion : IComandos
     {
         private List<string> tiposBusqueda = new List<string>();
         private List<Publicacion> resultado = new List<Publicacion>();
-        public BuscarPublicacion()
+        public EjecutarComando()
+        {
+
+        }
     }
 }
--- src/Library/Interfaces/IUsuario.cs
+++ src/Library/Interfaces/IUsuario.cs
@@ -1,19 +1,22 @@
-///<summary>
-/// Interfaz con los datos de usuario
-///</summary>
-public interface IUsuario
-
+namespace ConsoleApplication
 {
-    /// <summary>
-    /// id del usuario
-    /// </summary>
-    /// <value></value>
-    string id {get; set;}
-    /// <summary>
-    /// nombre del usuario
-    /// </summary>
-    /// <value></value>
-    string nombre {get; set;}
+    ///<summary>
+    /// Interfaz con los datos de usuario
+    ///</summary>
+    public interface IUsuario
+
+    {
+        /// <summary>
+        /// id del usuario
+        /// </summary>
+        /// <value></value>
+        string id {get; set;}
+        /// <summary>
+        /// nombre del usuario
+        /// </summary>
+        /// <value></value>
+        string nombre {get; set;}
 
 
+    }
 }
