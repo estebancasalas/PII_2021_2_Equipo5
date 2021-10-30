a3745eeaa02e76e3e9c1d7314eb675fd8c96ed8b
Ionasjospo <88661713+Ionasjospo@users.noreply.github.com>
2021-10-29 23:21:29 -0300

    Correccion de errores

---
+++ src/Library/Listas/ListaCodigosValidos.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace Library
+{
+    public  class listaCodigosValidos
+    {
+        public static List<string> CodigosValidos = new List<string> (); 
+    
+    }
+
+
+
+
+
+}
