97d753924b0c709666c98b7d5a6191ba212aaf8c
SantiRDL <santiago.rodriguezdl@correo.ucu.edu.uy>
2021-11-01 09:35:36 -0300

    Update MatrialesHandler.cs

--- src/Library/IHandlers/Handlers/MatrialesHandler.cs
+++ src/Library/IHandlers/Handlers/MatrialesHandler.cs
@@ -6,7 +6,7 @@ namespace Library
     public class MaterialesHandler  :IHandler
     {
         public IHandler Next {get; set;}
-        public void Handle(Mensaje mensaje)
+        /*public void Handle(Mensaje mensaje)
         {
             if (mensaje.Text.Contains("/"))
             {}
@@ -14,6 +14,25 @@ namespace Library
             {
                 this.Next.Handle(mensaje);
             }
+        }*/
+
+        public override void Handle(Mensaje mensaje)
+        {
+            Console.WriteLine("Cual es su nombre?");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Cual es su costo?");
+            string costo = Console.ReadLine();
+            Console.WriteLine("Cual es la cantidad?");
+            string cantidad = Console.ReadLine(); 
+            Console.WriteLine("Cual es la ubicación");
+            string ubicacion = Console.ReadLine(); //Hay que hacer clase para conseguir la ubicacion.
+            Console.WriteLine("Cuantas unidades?");
+            string unidad = Console.ReadLine();
+            Console.WriteLine("Posee alguna habilitación?");
+            string habilitaciones = Console.ReadLine();
+
+            DatosDeMateriales material = new Material(nombre, costo, cantidad, ubicacion, unidad, habilitaciones);
+
         }
     }
 }
