8c66d06408656df2f719e550768d6b43dc4e3cd4
Ionasjospo <88661713+Ionasjospo@users.noreply.github.com>
2021-11-02 01:48:52 -0300

    Comentarios

--- src/Library/Listas/ListaEmprendedores.cs
+++ src/Library/Listas/ListaEmprendedores.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace Library
 {
+    /// <summary>
+    /// /// Clase la cual contiene una lista en la cual estaran todos los emprendedores. Cumple con el patrón SRP ya que su única responsabilidad es conocer los emprendedores.
+    /// </summary>
     public class ListaEmprendedores 
     {
         public static List<Emprendedor> Emprendedores = new List<Emprendedor>();
