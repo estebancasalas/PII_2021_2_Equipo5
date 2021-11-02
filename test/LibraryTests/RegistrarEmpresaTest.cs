988b5ec71f187df833ac099ff56d6c691cb59132
estebancasalas <e.casalas@outlook.com>
2021-11-02 20:22:54 -0300

    Comienzo RegistrarEmpresarioTest

--- test/LibraryTests/RegistrarEmpresaTest.cs
+++ test/LibraryTests/RegistrarEmpresaTest.cs
@@ -15,23 +15,29 @@ namespace LibraryTests
     public class RegistrarEmpresaTests
     {
         Mensaje mensaje;
-        Administrador admin = new Administrador(456, "Esteban");
-        CrearEmpresaHandler handler = new CrearEmpresaHandler();
+        RegistrarEmpresarioHandler registrarEmpresario;
        
         [SetUp]
         public void Setup()
         {
-            mensaje.Text = "/CrearUsuario";
-            mensaje.Id = 1234;
-            admin.CrearInvitacion("Empresa1", "Montevideo", "textil", "9876");
+            ListaEmpresa lista1 = new ListaEmpresa();
 
         }
 
-
+        /// <summary>
+        /// En este test verificamos que, cuando la invitación es válida, el id del usuario se añade correctamente a la lista de 
+        /// ids de la empresa.
+        /// </summary>
         [Test]
-        public void Test()
+        public void InvitacionValidaTest()
         {
-            
+            ListaEmpresa lista1 = new ListaEmpresa();
+            Mensaje mensaje = new Mensaje(1234,"/empresa");
+            RegistrarEmpresarioHandler registrarEmpresario = new RegistrarEmpresarioHandler();
+            Administrador admin = new Administrador(456, "Esteban");
+            admin.CrearInvitacion("Empresa1", "Montevideo", "textil", "InviteEmpresa1");
+            registrarEmpresario.Handle(mensaje);
+            Assert.That(ListaEmpresa.Empresas[0].ListaIdEmpresarios.Contains(1234));
         }
     }
 }
