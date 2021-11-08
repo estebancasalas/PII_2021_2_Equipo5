a0f807f05b62e6a5bb2288d69d72c5ea781553e6
Ionasjospo <88661713+Ionasjospo@users.noreply.github.com>
2021-11-08 09:36:26 -0300

    cambios

---
+++ src/Library/Interfaces/IUsuario.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+namespace Library
+{
+    ///<summary>
+    /// Interfaz que contiene los datos utilizados por empresas y emprendedores.
+    /// Al ser una interfaz, se busca (a través del principio LSP) tener un programa que cumpla con el
+    /// principio OCP. Esto se logra al tener clases que implementen la interfaz, permitiendo extender
+    /// las capacidades del programa sin modificarlo. Se extienden al agregar otras clases que implementen
+    /// la interfaz y sustituyendo en el programa principal.
+    ///</summary>
+
+    public interface IUsuario
+    {
+        /// <summary>
+        /// Nombre de la empresa o emprendedor.
+        /// </summary>
+        /// <value></value>
+        string Nombre { get; set; }
+        /// <summary>
+        /// Rubro de la empresa o emprendedor.
+        /// </summary>
+        /// <value></value>
+        string Rubro { get; set; } 
+        /// <summary>
+        /// Ubicación de la empresa o emprendedor.
+        /// </summary>
+        /// <value></value>
+        string Ubicacion { get; set ;}
+    }
+
+}
